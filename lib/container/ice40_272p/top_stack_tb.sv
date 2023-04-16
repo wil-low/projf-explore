7c83d4612d7069315ab50875f582d73c2fc7980f
Andrei Ivushkin <aivushkin@gmail.com>
2023-04-16 12:06:04 +0300

    Stack fixes and testbench

--- lib/container/ice40_272p/top_stack_tb.sv
+++ lib/container/ice40_272p/top_stack_tb.sv
@@ -5,76 +5,144 @@ module top_stack_tb();
 
 parameter CLK_PERIOD = 10;  // 10 ns == 100 MHz
 localparam WIDTH = 8;
-localparam DEPTH = 4;
+localparam STACK_DEPTH = 4;
 
 logic clk;
 
 // generate clock
 always #(CLK_PERIOD / 2) clk <= ~clk;
 
-logic rst_n;						// reset
+logic stack_rst_n = 1;
+logic stack_push_en = 0;			// push enable (add on top)
+logic stack_pop_en = 0;				// pop enable (remove from top)
+logic stack_peek_en = 0;			// peek enable (return item at index, no change)
+logic stack_poke_en = 0;			// poke enable (replace item at index)
+logic [55:0] stack_data_in;			// data to push|poke
+logic [55:0] stack_data_out; 		// data returned for pop|peek
+logic stack_full;					// buffer is full
+logic stack_empty;					// buffer is empty
+logic [$clog2(STACK_DEPTH):0] stack_index;  // element index t (0 is top)
+logic [$clog2(STACK_DEPTH):0] stack_depth;  // returns how many items are in stack
 
-logic push_en = 0;					// push enable (port a)
-logic [WIDTH-1:0] push_data;		// data to push (port a)
-
-logic pop_en = 0;		  			// pop enable (port a)
-logic [WIDTH-1:0] pop_data; 		// data to pop (port b)
-
-wire logic full;					// buffer is full
-wire logic empty;					// buffer is empty
-
-stack #(.WIDTH(WIDTH), .DEPTH(DEPTH))
+stack #(.WIDTH(8), .DEPTH(STACK_DEPTH))
 stack_inst(
-	.clk_write(clk), .clk_read(clk), .rst_n(rst_n),
-	.push_en, .push_data, .pop_en, .pop_data,
-	.full, .empty
+	.clk,
+	.rst_n(stack_rst_n),
+	.push_en(stack_push_en),
+	.pop_en(stack_pop_en),
+	.peek_en(stack_peek_en),
+	.poke_en(stack_poke_en),
+	.index(stack_index),
+	.data_in(stack_data_in),
+	.data_out(stack_data_out),
+	.full(stack_full),
+	.empty(stack_empty),
+	.depth(stack_depth)
 );
 
 initial begin
 	$dumpfile("top_stack_tb.vcd");
 	$dumpvars(0, top_stack_tb);
 	clk = 1;
-	push_en = 0;
-	pop_en = 0;
+	stack_push_en = 0;
+	stack_pop_en = 0;
 
-	rst_n = 1;
-	#CLK_PERIOD rst_n = 0;
-	#CLK_PERIOD rst_n = 1;
-
-	#CLK_PERIOD;
-	push_data = 1;
-	push_en = 1;
+	stack_rst_n = 1;
+	#CLK_PERIOD stack_rst_n = 0;
+	#CLK_PERIOD stack_rst_n = 1;
 
 	#CLK_PERIOD;
-	push_data = 2;
-	push_en = 1;
+	stack_data_in = 1;
+	stack_push_en = 1;
 
 	#CLK_PERIOD;
-	push_data = 3;
-	push_en = 1;
+	stack_data_in = 2;
+	stack_push_en = 1;
 
 	#CLK_PERIOD;
-	push_data = 4;
-	push_en = 1;
+	stack_data_in = 3;
+	stack_push_en = 1;
 
 	#CLK_PERIOD;
-	push_data = 5;
-	push_en = 1;
+	stack_data_in = 4;
+	stack_push_en = 1;
 
 	#CLK_PERIOD;
-	push_data = 6;
-	push_en = 1;
+	stack_data_in = 5;
+	stack_push_en = 1;
 
 	#CLK_PERIOD;
-	push_data = 7;
-	push_en = 1;
+	stack_data_in = 6;
+	stack_push_en = 1;
 
 	#CLK_PERIOD;
-	push_data = 0;
-	push_en = 0;
+	stack_data_in = 7;
+	stack_push_en = 1;
 
 	#CLK_PERIOD;
-	pop_en = 1;
+	stack_push_en = 0;
+
+	#CLK_PERIOD;
+	stack_index = 0;
+	stack_peek_en = 1;
+
+	#CLK_PERIOD;
+	stack_index = 1;
+	stack_peek_en = 1;
+
+	#CLK_PERIOD;
+	stack_index = 2;
+	stack_peek_en = 1;
+
+	#CLK_PERIOD;
+	stack_index = 3;
+	stack_peek_en = 1;
+
+	#CLK_PERIOD;
+	stack_index = 4;
+	stack_peek_en = 1;
+
+	#CLK_PERIOD;
+	stack_peek_en = 0;
+
+	#CLK_PERIOD;
+	stack_index = 1;
+	stack_data_in = 'hff;
+	stack_poke_en = 1;
+
+	#CLK_PERIOD;
+	stack_poke_en = 0;
+	stack_index = 1;
+	stack_peek_en = 1;
+
+	#CLK_PERIOD;
+	stack_index = 0;
+	stack_peek_en = 1;
+
+	#CLK_PERIOD;
+	stack_index = 1;
+	stack_peek_en = 1;
+
+	#CLK_PERIOD;
+	stack_index = 2;
+	stack_peek_en = 1;
+
+	#CLK_PERIOD;
+	stack_index = 3;
+	stack_peek_en = 1;
+
+	#CLK_PERIOD;
+	stack_index = 4;
+	stack_peek_en = 1;
+
+	#CLK_PERIOD;
+	stack_peek_en = 0;
+	
+	#CLK_PERIOD;
+	stack_rst_n = 0;
+
+	#CLK_PERIOD;
+	stack_rst_n = 1;
 
 	#300 $finish;
 end
--- lib/container/stack.sv
+++ lib/container/stack.sv
@@ -11,49 +11,51 @@ module stack
 
 	input logic rst_n,					// reset
 
-	input logic push_en,			  	// push enable (port a)
-	input logic [WIDTH-1:0] push_data,	// data to push (port a)
+	input logic push_en,			  	// push enable (add on top)
+	input logic pop_en,			  		// pop enable (remove from top)
+	input logic peek_en,			  	// peek enable (return item at index, no change)
+	input logic poke_en,			  	// poke enable (replace item at index)
 
-	input logic pop_en,			  	// pop enable (port a)
-	output logic [WIDTH-1:0] pop_data, 		// data to pop (port b)
+	input logic [WIDTH-1:0] data_in,	// data to push|poke
+	input logic [$clog2(DEPTH):0] index,	// element index t (0 is top)
+
+	output logic [WIDTH-1:0] data_out, 	// data returned for pop|peek
 
 	output logic full,					// buffer is full
 	output logic empty,					// buffer is empty
-	output logic [$clog2(DEPTH):0] item_count	// buffer is empty
+	output logic [$clog2(DEPTH):0] depth	// returns how many items are in stack
 );
 
-logic [$clog2(DEPTH) - 1:0] addr_write, addr_read;
-logic do_push;
+logic [$clog2(DEPTH):0] addr_write;
 
-assign full = addr_write == DEPTH;
+logic [$clog2(DEPTH):0] addr_write_proxy, addr_read_proxy;
+logic we_proxy;
+
+assign we_proxy = (push_en && !full) || (poke_en && index < depth);
+assign addr_write_proxy = poke_en ? addr_write - index - 1: addr_write;
+assign addr_read_proxy = peek_en ? addr_write - index - 1: addr_write - 1;
+
+assign full = addr_write >= DEPTH;
 assign empty = addr_write == 0;
-
-assign item_count = addr_write;
+assign depth = addr_write;
 
 bram_sdp #(.WIDTH(WIDTH), .DEPTH(DEPTH))
 bram_sdp_inst (
-	.clk_write(clk), .clk_read(clk), .we(do_push),
-	.addr_write(addr_write), .addr_read(addr_read),
-	.data_in(push_data), .data_out(pop_data)
+	.clk_write(clk), .clk_read(clk), .we(we_proxy),
+	.addr_write(addr_write_proxy), .addr_read(addr_read_proxy),
+	.data_in(data_in), .data_out(data_out)
 );
 
 always_ff @(posedge clk) begin
 	if (!rst_n) begin
+		$display("stack rst_n");
 		addr_write <= 0;
 	end
 	else begin
-		if (push_en && !full) begin
+		if (push_en && !full)
 			addr_write <= addr_write + 1;
-			addr_read <= addr_write;
-			do_push <= 1;
-		end
-		else begin
-			do_push <= 0;
-		end
-		if (pop_en && !empty) begin
+		else if (pop_en && !empty)
 			addr_write <= addr_write - 1;
-			addr_read <= addr_read - 1;
-		end
 	end
 end
 
