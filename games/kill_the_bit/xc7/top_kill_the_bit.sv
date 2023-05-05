86ce1382d455673b9165b6d11d33a2859acb09f4
Andrei Ivushkin <aivushkin@gmail.com>
2023-05-05 15:56:33 +0300

    kill_the_bit working on xc7

--- games/kill_the_bit/xc7/top_kill_the_bit.sv
+++ games/kill_the_bit/xc7/top_kill_the_bit.sv
@@ -4,7 +4,6 @@
 module top_kill_the_bit
 (
 	input wire CLK,
-	input wire rst_n,
 	
 	output logic LK_CLK,
 	inout  wire  LK_DIO,
@@ -13,9 +12,20 @@ module top_kill_the_bit
 	output logic [7:0] LED
 );
 
+//// Reset emulation for ice40
+logic [22:0] reset_counter = 0;
+logic rst_n = &reset_counter;
+
+logic [11:0] trace;
+
+always @(posedge CLK) begin
+	if (!rst_n)
+		reset_counter <= reset_counter + 1;
+end
+
 kill_the_bit
 #(
-	.CLOCK_FREQ_MHz(25)
+	.CLOCK_FREQ_MHz(50)
 )
 kill_the_bit_inst
 (
