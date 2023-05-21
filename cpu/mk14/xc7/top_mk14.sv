e3f4544c4eb1e9b810f734e793a347c8dc4cb554
Andrei Ivushkin <aivushkin@gmail.com>
2023-05-21 22:03:40 +0300

    Project updates

--- cpu/mk14/xc7/top_mk14.sv
+++ cpu/mk14/xc7/top_mk14.sv
@@ -14,7 +14,9 @@ module top_mk14
 
 	output logic LK_CLK,
 	output logic LK_STB,
-	inout  wire  LK_DIO
+	inout  wire  LK_DIO,
+
+	input wire IR
 );
 
 //// Reset emulation
@@ -31,9 +33,9 @@ assign LED = ~trace;
 
 localparam CLOCK_FREQ_MHZ = 50;
 
-//localparam ROM_INIT_F		= "SCIOS_Version_2.mem";
-localparam ROM_INIT_F		= "display.mem";
-localparam STD_RAM_INIT_F	= "test.mem";
+localparam ROM_INIT_F		= "SCIOS_Version_2.mem";
+localparam STD_RAM_INIT_F	= "collatz.mem";
+//localparam STD_RAM_INIT_F	= "test.mem";
 localparam EXT_RAM_INIT_F	= "ext_ram.mem";
 
 mk14_soc #(
@@ -49,7 +51,8 @@ mk14_soc_inst (
 	.probe(PROBE),
 	.o_ledkey_clk(LK_CLK),
 	.o_ledkey_stb(LK_STB),
-	.io_ledkey_dio(LK_DIO)
+	.io_ledkey_dio(LK_DIO),
+	.ir(IR)
 );
 
 endmodule
