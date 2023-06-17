38fc38e231a93b0023bbb08163d1dfd40210aab3
Andrei Ivushkin <aivushkin@gmail.com>
2023-06-17 12:12:12 +0300

    Support for readmemb

--- lib/memory/rom_async.sv
+++ lib/memory/rom_async.sv
@@ -6,20 +6,25 @@
 `timescale 1ns / 1ps
 
 module rom_async #(
-    parameter WIDTH=8,
-    parameter DEPTH=256,
-    parameter INIT_F=""
-    ) (
-    input wire logic [$clog2(DEPTH)-1:0] addr,
-    output     logic [WIDTH-1:0] data
-    );
+    parameter WIDTH = 8,
+    parameter DEPTH = 256,
+    parameter INIT_F = "",
+    parameter BIN_MODE = 0
+)
+(
+	input wire logic [$clog2(DEPTH)-1:0] addr,
+	output     logic [WIDTH-1:0] data
+);
 
     logic [WIDTH-1:0] memory [DEPTH];
 
     initial begin
         if (INIT_F != 0) begin
-            $display("Creating rom_async from init file '%s'.", INIT_F);
-            $readmemh(INIT_F, memory);
+            $display("Creating rom_async from init file '%s', bin_mode %d", INIT_F, BIN_MODE);
+			if (BIN_MODE)
+	            $readmemb(INIT_F, memory);
+			else
+	            $readmemh(INIT_F, memory);
         end
     end
 
