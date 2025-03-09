5655450bf5aac10d1ddd9f05c7264cd821e9fb2d
Andrei Ivushkin <aivushkin@gmail.com>
2025-03-09 09:15:34 +0200

    mk14 upgraded to new Gowin EDA version

--- cpu/mk14/upload_ram_gowin.sh
+++ cpu/mk14/upload_ram_gowin.sh
@@ -7,3 +7,7 @@ TTY=/dev/ttyUSB1
 
 stty -F $TTY speed 115200 cs8 -cstopb -parenb
 cat $HEXFILE > $TTY && echo $HEXFILE uploaded to $TTY
+
+
+# program FPGA:
+#  openFPGALoader -b tangnano9k gowin/mk14/impl/pnr/mk14.fs
